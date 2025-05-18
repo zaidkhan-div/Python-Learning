e9999f45f7dbc7e1babd27cfef15071438f8e90a
zaidkhan-div <zaidscestudent@gmail.com>
2025-05-18 15:44:22 +0500

    Definations In Python

---
+++ Python/1-Basic of Python/def.py
@@ -0,0 +1,42 @@
+
+# name=lambda name:print(name)
+
+# name("talha")#
+# name=lambda name:print(name)
+
+# name("talha") def hello():
+#     print("hello world")
+
+# hello()
+
+# def hello(name="talha"):
+#     print(f"hello {name}")
+
+
+# hello()
+
+
+# name=lambda name:print(name)
+
+# name("talha
+
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
+    
+#     return n * fact(n-1)
+
+# print(fact(5))
+
+
+# def myfunc(*args):
+#     return sum(args)
+
+# print(myfunc(2,2,2,2,2,2))
+
+
+def kfunc(**kwargs):
+    print(kwargs)
+
+
+kfunc(name="noman")
