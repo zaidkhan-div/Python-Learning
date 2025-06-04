bcac2332b85570fcc5226a5c5397b59e93a6c45a
zaidkhan-div <zaidscestudent@gmail.com>
2025-06-05 00:58:48 +0500

    Oops in Js

---
+++ TypeScript/oops/src/index.ts
@@ -0,0 +1,11 @@
+// Oops Learning in TypeScript 
+// Classes 
+
+class User {
+    userName: string;
+    userAge: number;
+    constructor(name: string, age: number) {
+        this.userName = name;
+        this.userAge = age;
+    }
+}
