d042ea0955a2d454bf0a8894c08076010892fb19
zaidkhan-div <zaidscestudent@gmail.com>
2025-06-17 14:17:17 +0500

    .

--- JavaScript/JsConcepts/Objects&Array.js
+++ JavaScript/JsConcepts/Objects&Array.js
@@ -13,3 +13,14 @@ const result = Object.assign(target, source1, source2);
 
 console.log(result);  // Output: { a: 1, b: 3, c: 5, d: 6 }
 console.log(target);  // Output: { a: 1, b: 3, c: 5, d: 6 }
+
+const target1 = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target1, source);
+
+console.log(target1);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget === target1);
+// Expected output: true
