ae0cba11b51fd0307864a6e24f35da5898454474
zaidkhan-div <zaidscestudent@gmail.com>
2025-06-24 12:24:08 +0500

    Optional and Nullish coalescing

--- Javascript/JsConcepts/Objects&Array.js
+++ Javascript/JsConcepts/Objects&Array.js
@@ -24,3 +24,17 @@ console.log(target1);
 
 console.log(returnedTarget === target1);
 // Expected output: true
+
+// ?. Optional Chaining
+// Purpose: Safely access deeply nested properties without causing an error if a property is null or undefined.
+const user = { profile: null };
+console.log(user.profile?.name); // undefined (does NOT throw error)
+
+// ?? — Nullish Coalescing
+// Purpose: Provide a default value when the left-hand side is null or undefined (but not when it’s 0, false, or '').
+
+let names = null;
+console.log(names ?? "Default"); // "Default"
+
+let score = 0;
+console.log(score ?? 10); // 0 (not 10!)
