ee0761f6a7f6fcfb3c3d9f177d5035f0fe9ca9f7
zaidkhan-div <zaidscestudent@gmail.com>
2025-07-07 23:53:28 +0500

    .

--- Javascript/Logics/app.js
+++ Javascript/Logics/app.js
@@ -1,4 +1,19 @@
-function addTwoNums(a, b) {
-    return a + b;
+// function addTwoNums(a, b) {
+//     const addTwoNums = a + b;
+//     console.log('Sum of two is', addTwoNums)
+// }
+// addTwoNums(1.5, 1.5);
+
+
+function calculateArea(length, width) {
+    if (length <= 0) {
+        throw new RangeError('Length should be a positive number');
+    }
+    if (width <= 0) {
+        throw new RangeError('Width should be a positive number')
+    }
+    const calculateArea = length * width;
+    return calculateArea;
 }
-console.log(addTwoNums(1,2));
+let result = calculateArea(2, -3);
+console.log(result)
