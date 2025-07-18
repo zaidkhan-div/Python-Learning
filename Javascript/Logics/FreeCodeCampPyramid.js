6e370b64605f0b32bf57cc31d8880ae7c7d6dad0
zaidkhan-div <zaidscestudent@gmail.com>
2025-07-18 19:04:25 +0500

    .

--- Javascript/Logics/FreeCodeCampPyramid.js
+++ Javascript/Logics/FreeCodeCampPyramid.js
@@ -47,3 +47,29 @@ console.log(result);
 
 // false, 0, "", null, undefined, and NaN. Javascript consider them false  
 
+
+
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
