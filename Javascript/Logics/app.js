0f05a383a14c43122b73a013c5cce5f150c78c7a
zaidkhan-div <zaidscestudent@gmail.com>
2025-07-24 18:23:26 +0500

    .

--- Javascript/Logics/app.js
+++ Javascript/Logics/app.js
@@ -7,16 +7,17 @@
 // addTwoNums(1.5, 1.5);
 
 
-function calculateArea(length, width) {
-    if (length <= 0) {
-        throw new RangeError('Length should be a positive number');
-    }
-    if (width <= 0) {
-        throw new RangeError('Width should be a positive number')
-    }
-    const calculateArea = length * width;
-    return calculateArea;
-}
+// function calculateArea(length, width) {
+//     if (length <= 0) {
+//         throw new RangeError('Length should be a positive number');
+//     }
+//     if (width <= 0) {
+//         throw new RangeError('Width should be a positive number')
+//     }
+//     const calculateArea = length * width;
+//     return calculateArea;
+// }
+
 // let result = calculateArea(2, -3);
 
 // function findSmallestInt(arr) {
@@ -37,3 +38,20 @@ function calculateArea(length, width) {
 
 // const result = findMedianSortedArrays([1, 2], [3, 4]);
 // console.log(result)
+
+
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    // "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    // "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
+  }
+];
+console.log(locations["button text"[0]]);
