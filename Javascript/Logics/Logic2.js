51a943d6f83ee89ee998233ce3e04ac264040ba5
zaidkhan-div <zaidscestudent@gmail.com>
2025-09-02 15:33:04 +0500

    .

--- Javascript/Logics/Logic2.js
+++ Javascript/Logics/Logic2.js
@@ -1,4 +1,5 @@
 function calculateFactorial(input) {
+  if (input < 0) throw new RangeError("Number should be positive");
   let result = 1;
   for (let i = 0; i < input; i++) {
     result += result * i;
@@ -6,9 +7,20 @@ function calculateFactorial(input) {
   }
   return result
 }
-console.log(calculateFactorial(4));
+// console.log(calculateFactorial(4));
 
-// ======== vowels Count ====
+// Factorial using recursion
+function calculateFactusingRecursion(num) {
+  if (num < 0) throw new RangeError("Number should be positive");
+  if (num === 0 || num === 1) {
+    return 1
+  }
+  return num * calculateFactorial(num - 1)
+}
+console.log(calculateFactorial(6));
+
+
+// ======== vowels Count =====
 
 let str = "HEY JS YOU ARe amazing";
 let vowels = ["a", "e", "i", "o", "u"];
@@ -43,4 +55,22 @@ function sum(num1, num2) {
   num2 = 5; // here we can overwrite the arguments untill we use strit mode
   return arguments[0] + arguments[1];
 }
-console.log(sum(2, 3), "Sum-Func")
+// console.log(sum(2, 3), "Sum-Func");
+
+// count each character repeatition
+let stringArr = "lorem ipsum dolar dollar nothing but random string";
+let storeStringObj = {}
+for (const x of stringArr) {
+  // storeStringObj[x] this si the dynamic key = 1
+  if (storeStringObj[x]) {
+    storeStringObj[x] += 1
+  } else {
+    storeStringObj[x] = 1
+  }
+}
+// console.log(storeStringObj, "Obj");
+
+
+const checkingTrue = false;
+const checkresult = !!checkingTrue;// !! this will return boolean for anyValues
+console.log(checkresult, "CheckResult"); 
