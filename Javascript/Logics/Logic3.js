d28e929891d604670312aee7e438df6cb11e4dbb
zaidkhan-div <zaidscestudent@gmail.com>
2025-10-16 12:53:09 +0500

    .

--- Javascript/Logics/Logic3.js
+++ Javascript/Logics/Logic3.js
@@ -90,4 +90,14 @@ function sudoku(puzzle) {
     return puzzle
 }
 
-console.log(sudoku(puzzle));
+// console.log(sudoku(puzzle));
+
+
+console.log("A");
+setTimeout(() => {
+    console.log("B")
+}, 0);
+Promise.resolve().then(() => console.log("C"));
+console.log("D")
+
+
