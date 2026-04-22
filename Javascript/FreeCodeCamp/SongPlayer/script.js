09f8c307c5232a9acc5f826cb90cc60c8dda81eb
zaidkhan-div <zaidscestudent@gmail.com>
2026-04-22 16:27:57 +0500

    building Song player on freeCodeCamp

--- Javascript/FreeCodeCamp/SongPlayer/script.js
+++ Javascript/FreeCodeCamp/SongPlayer/script.js
@@ -209,8 +209,18 @@ const renderSongs = (array) => {
 
     resetButton.id = "reset";
     resetButton.ariaLabel = "Reset playlist";
-       
-  }
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+
+      renderSongs(sortSongs()); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+
+  };
 
 };
 
@@ -241,6 +251,20 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+
+
+    }
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
@@ -258,3 +282,4 @@ const sortSongs = () => {
 };
 
 renderSongs(sortSongs());
+setPlayButtonAccessibleText();
